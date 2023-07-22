055aa2a72bda8343dace4e1a2be784f1bfc5b1a8
Ram Mishra <ramm59128@gmail.com>
2023-07-22 07:32:29 +0530

    Add files via upload

---
+++ easy-folder-locker/src/main/java/com/develpoerram/easyfolderlocker/Ram.java
@@ -0,0 +1,18 @@
+package com.developerram.easyfolderlocker;
+
+import com.developerram.easyfolderlocker.ui.MainUI;
+
+/**
+ *
+ * @author Ram Mishra
+ */
+public class RamFileMishra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new MainUI().setApp();       
+    }
+    
+}
